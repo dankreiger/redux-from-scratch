1e067d451ed9f7791ea74a53874666a4999d4b07
Dan Kreiger <dan@dankreiger.com>
2019-08-20 01:15:00 +0200

    chore(*): import domListeners

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 import "./lib";
 import "./src/scss";
+import "./domListeners";
 import store from "./src/store";
 
 window.store = store;
-
-document.querySelector("input").addEventListener("input", e => {
-  store.dispatch({ type: "UPDATE_WOOFER", payload: e.target.value });
-});
 
 const render = () => {
   document.getElementById(
