a7ae10fac56ed40d08e4cf0bf44f734416c4b8b8
Dan Kreiger <dan@dankreiger.com>
2019-08-20 01:20:32 +0200

    chore(*): remove debugger

--- domListeners.js
+++ domListeners.js
@@ -1,5 +1,4 @@
 import store from "./src/store";
 document.querySelector("input").addEventListener("input", e => {
-  console.log(e.target.value);
   store.dispatch({ type: "UPDATE_WOOFER", payload: e.target.value });
 });
